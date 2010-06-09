54260f8cb49977641732236bd4d927967d74afa9
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-06-09 17:29:43 +0200

    prototypes des fonctions d'édition

---
+++ editeur.php
@@ -0,0 +1,39 @@
+<?php
+
+html_formulaire_edition() {
+}
+
+créer_article(article_parent) {
+    // Crée le dossier de cet article
+    // Crée un fichier index pour le contenu de cet article à partir du modèle du dossier parent
+    // Crée un fichier modèle pour les nouveaux sous-articles
+}
+
+supprimer_article(article, récursif) {
+}
+
+liste_sous_articles(article) {
+}
+
+get_modèle(article) {
+}
+
+set_modèle(article, contenu) {
+}
+
+get_article(article) {
+}
+
+set_article(article, contenu) {
+}
+
+// TODO :
+Pouvoir ajouter des propriétés aux articles :
+sous_article/index
+            /modèle
+            /prop_photo
+            /prop_date
+            /prop_lieu
+            /sous_article_1
+            /sous_article_2
+?>
