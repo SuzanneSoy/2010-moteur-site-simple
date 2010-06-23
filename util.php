cdf7a1090f6f57fb7fba65df7203246041c98b03
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-06-23 18:54:50 +0200

    Renomage de fonction dans util.php (utilisée nulle par ailleurs actuellement)

--- util.php
+++ util.php
@@ -3,7 +3,7 @@
 // Fonctions utilitaires.
 
 // Concatène deux chemins.
-function concat_path($p1, $p2) {
+function concaténer_chemin($p1, $p2) {
     return $p1 . '/' . $p2; // FIXME : . '/' . n'est pas portable !
 }
 
