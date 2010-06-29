451a2f18adad91ea6d7495a6bfc17db346f8c56e
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-06-29 05:36:57 +0200

    Ajout de la classe GalerieEvenement.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 require_once("types/galerie.php");
 require_once("types/galerie-periode.php");
+require_once("types/galerie-evenement.php");
 
 $p = Page::_new("/modele/galerie");
 
---
+++ types/galerie-evenement.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("controleur/page.php");
+
+class GalerieEvenement extends Page {
+    public function vue($nom_vue = "normal") {
+        $ret = '';
+        
+        $ret .= '<ul>';
+        foreach ($this->liste_enfants() as $k) {
+            $ret .= '<li><a href="' . $k->url() . '">' . '['.$k->chemin->get().'] ' . $k->vue('miniature') . '</a></li>';
+            
+            if ($nom_vue == "miniature") {
+                break;
+            }
+        }
+        $ret .= '</ul>';
+        
+        if ($nom_vue == "edition") {
+            $ret .= '<a href="' . $this->url('nouveau') . '">Nouvel évènement.</a>';
+        }
+        
+        return $ret;
+    }
+  
+    // TODO :
+    // Chaque page est un objet PHP, avec les méthodes suivantes :
+    // $page->vue(nom_vue, nom_vue_fallback_1, nom_vue_fallback_2, …); // nom_vue = normal, edition, miniature, …
+    //
+    // + Méthodes définies dans modele/page.php
+}
+
+Page::ajouterType("GaleriePeriode", "GaleriePeriode");
+
+?>
