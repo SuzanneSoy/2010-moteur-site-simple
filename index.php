b2f6a1f46bf05c6322080b4fe9cd37a39833730b
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-06-29 05:49:07 +0200

    Ajout de la classe GaleriePhoto.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 require_once("types/galerie.php");
 require_once("types/galerie-periode.php");
 require_once("types/galerie-evenement.php");
+require_once("types/galerie-photo.php");
 
 $p = Page::_new("/modele/galerie");
 
---
+++ types/galerie-photo.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("controleur/page.php");
+
+class GaleriePhoto extends Page {
+    public function vue($nom_vue = "normal") {
+        if ($nom_vue == "miniature") {
+            return '<img src="' . $this->get_prop("image_mini") . '"/>';
+        }
+        
+        if ($nom_vue == "edition") {
+            return '<a href="' . $this->url('nouveau') . '">Changer la photo</a>';
+        }
+
+        return '<img src="' . $this->get_prop("image") . '"/>';
+    }
+}
+
+Page::ajouterType("GaleriePhoto", "GaleriePhoto");
+
+?>
