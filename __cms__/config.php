f648049f389ec6a89c7536598a69d75f60cae11a
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-07-02 22:33:25 +0200

    Bug dû au déplacement des fichiers corrigé.

--- __cms__/config.php
+++ __cms__/config.php
@@ -3,7 +3,7 @@
 require_once("util.php");
 
 $config_url_base = "http://" . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
-$config_chemin_base = dirname($_SERVER[SCRIPT_FILENAME]);
+$config_chemin_base = dirname(__FILE__);
 
 $config_chemin_modele = concaténer_chemin_fs($config_chemin_base, "/modele");
 ?>
