0e58eb753ace99e1b109053b79d15ba041ce5368
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-07-02 23:44:32 +0200

    Revert "Détection automatique de l'URL de base du site."
    This reverts commit 429b33ea647ba92d50293f953cfc93f36a678c2d.
    
    Conflicts:
    
    	__cms__/config.php

--- __cms__/config.php
+++ __cms__/config.php
@@ -2,7 +2,7 @@
 
 require_once("util.php");
 
-$config_url_base = "http://" . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
+$config_url_base = "http://127.0.0.1/2010-moteur-site-simple/";
 $config_chemin_base = dirname(__FILE__);
 
 $config_chemin_modele = concaténer_chemin_fs($config_chemin_base, "/modele");
