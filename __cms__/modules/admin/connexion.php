0012f6b57cfc59a3be4db549e593a2cd39facfc1
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-07-30 03:27:09 +0200

    Remplissage du code des fonctions de Connexion.

--- __cms__/modules/admin/connexion.php
+++ __cms__/modules/admin/connexion.php
@@ -1,28 +1,45 @@
 <?php
 
-function action($chemin, $action, $paramètres) {
-	if ($action == "connexion") {
-		if (connexion($paramètres["utilisateur"], $paramètres["mdp"])) {
-			// Afficher la page "Connexion réussie" et un lien vers la page d'accueil.
+class Connexion {
+	public function action($chemin, $action, $paramètres) {
+		if ($action == "connexion") {
+			if (connexion($paramètres["utilisateur"], $paramètres["mdp"])) {
+				return self::vue("connexion réussie");
+			} else {
+				return self::vue("connexion échouée");
+			}
+		} else if ($action == "déconnexion") {
+			déconnexion();
+			return self::vue("déconnexion");
 		} else {
-			// Afficher "Mauvais mot de passe ou nom d'utilisateur" puis le formulaire de connexion.
-		}
-	} else if ($action == "déconnexion") {
-		déconnexion();
-		// Afficher "déconnexion réussie" et un lien vers la page d'accueil.
-	} else {
-		// Afficher le formulaire de connexion.
-		if (is_set($paramètres["vue"])) {
-			self::vue($chemin, $paramètres["vue"]);
-		} else {
-			self::vue($chemin);
+			if (is_set($paramètres["vue"])) {
+				self::vue($chemin, $paramètres["vue"]);
+			} else {
+				self::vue($chemin);
+			}
 		}
 	}
-}
-
-function vue($chemin, $vue = "normal") {
-	// Les quatre vues ("connexion réussie", "déconnexion réussie",
-	// formulaire de connexion, formulaire + "mauvais mdp")
+	
+	// TODO : Peut-être mettre ces textes dans un autre fichier ?
+	// TODO : $config_url_base n'est pas forcément la page d'accueil...
+	public function vue($chemin, $vue = "normal") {
+		// Les quatre vues ("connexion réussie", "déconnexion réussie",
+		// formulaire de connexion, formulaire + "mauvais mdp")
+		if ($vue == "normal") {
+			return formulaire_connexion();
+		} else if ($vue == "connexion réussie") {
+			return "<h1>Connexion réussie</h1><p>Pour vous déconnecter, utilisez le lien «déconnexion» en haut à droite.</p><p><a href=\"" . $config_url_base . "\">Retour à la page d'accueil</a>.</p>";
+		} else if ($vue == "connexion échouée") {
+			return formulaire_connexion("<p><strong>Mauvais mot de passe et/ou nom d'utilisateur. Ré-essayez ou retournez à la <a href=\"" . $config_url_base . "\">page d'accueil</a>.</strong></p>");
+		} else if ($vue == "déconnexion") {
+			return "<h1>Déconnexion réussie</h1><p>Vous êtes déconnecté. Vous pouvez à présent retourner à la <a href=\"" . $config_url_base . "\">page d'accueil</a>.</p>"
+		}
+	}
+	
+	public function formulaire_connexion($message = "") {
+		// TODO
+		return "<h1>Connexion</h1>" . $message . "<input type=\"text\" name=\"nom\" value=\"Nom\" />...";
+	}
 }
 
 ?>
