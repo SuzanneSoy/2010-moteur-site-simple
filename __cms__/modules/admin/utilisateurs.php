d95963834646b5e7ef4717c55176b511705bb9d5
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-08-01 00:57:07 +0200

    Possibilité de supprimer un utilisateur et brouillon de la liste des utilisateurs (HTML).

--- __cms__/modules/admin/utilisateurs.php
+++ __cms__/modules/admin/utilisateurs.php
@@ -19,6 +19,7 @@ class AdminUtilisateurs {
 		// Solution B :
 		} else {
 			if (is_set($paramètres["nouveau_nom"]) && ($action == "nouvelle_page")) {
+				// TODO : SECURITE : Si l'utilisateur existe déjà, laisser tomber et ne pas faire les set_* qui suivent !
 				Authentification::nouvel_utilisateur($paramètres["nouveau_nom"]);
 				$paramètres["nom"] = $paramètres["nouveau_nom"];
 				// TODO : message de confirmation quelque part ?
@@ -41,6 +42,11 @@ class AdminUtilisateurs {
 				Authentification::set_peut_se_connecter($paramètres["nom"], ($paramètres["peut_se_connecter"] == "true"));
 			}
 			
+			if (is_set($paramètres["nom"]) && ($action == "supprimer")) {
+				Authentification::supprimer_utilisateur($paramètres["nom"]);
+				// TODO : message de confirmation quelque part ?
+			}
+			
 			if (is_set($paramètres["vue"])) {
 				self::vue($chemin, $paramètres["vue"]);
 			} else {
@@ -57,10 +63,20 @@ class AdminUtilisateurs {
 			if (vérifier_permission($chemin, "nouvelle_page", get_utilisateur())) {
 				// afficher le lien "Nouvel utilisateur"
 			}
-	        $ret .= '<table class="utilisateurs index"><thead><th>Nom</th><th>Prénom</th><th>Groupe</th><th>Mot de passe</th></thead><tbody>';
+	        $ret .= '<table class="utilisateurs index"><thead><th>Nom</th><th>Prénom</th><th>Groupe</th><th>Mot de passe</th><th>Peut se connecter</th><th colspan="2"></th></thead><tbody>';
 	        $listegroupes = // Construire la liste des groupes sous forme de menu drop-down.
-	        foreach (stockage::liste_enfants($chemin) as $k) { // TODO : trier par ordre alphabétique du nom ?
-	            $ret .= '<tr>' . modules::vue($k) . '</tr>'; // ??? TODO
+	        foreach (Authentification::liste_utilisateurs()) {
+	            $ret .= '<tr>'
+					. '<form action="' . $chemin->get_url() . '">'
+					. '<td>' . $k . '</td>' // TODO : Nom
+					. '<td>' . $k . '</td>' // TODO : Prénom
+					. '<td>' . Authentification::get_groupe($k) . '</td>'
+					. '<td>' . Authentification::get_mot_de_passe($k) . '<input type="submit" value="Générer un nouveau mot de passe"/></td>'
+					. '<td>' . Authentification::get_peut_se_connecter($k) . '</td>'
+					. '<td><input type="submit" value="appliquer"/></td>'
+					. '<td><input type="submit" value="supprimer"/></td>' // TODO
+					. '</form>'
+					. '</tr>';
 	            // Le champ mot de passe doit être un lien / bouton "nouveau
 	            // mot de passe automatique" qui redirige vers
 	            // $chemin->enfant("$utilisateur") ?action=gen_mdp .
