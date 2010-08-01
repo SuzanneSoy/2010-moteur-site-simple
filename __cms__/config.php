925d551e0ccccea75b4acdc1eb6269f67cc4532a
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-08-01 05:27:07 +0200

    Mise à jour des noms de variables de config par rapport à ceux utilisés dans le code.

--- __cms__/config.php
+++ __cms__/config.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once("util.php");
-
 $config_url_base = "http://127.0.0.1/2010-moteur-site-simple/";
 
 $config_chemin_base = dirname(__FILE__);
-$config_chemin_modele = concaténer_chemin_fs($config_chemin_base, "/modele");
+$config_chemin_base_stockage = $config_chemin_base . "/modele";
+$config_chemin_base_public = $config_chemin_base . "/.."; // TODO !!!
+
 ?>
