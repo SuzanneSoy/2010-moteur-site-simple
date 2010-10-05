bae7ebbf9e81132aa2b0ad5b245f83fc2d21f517
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-10-05 16:27:04 +0200

    Revert des deux commit précédents

--- cms2/code/main.php
+++ cms2/code/main.php
@@ -1,16 +1,10 @@
 <?php
 
-require_once(dirname(__FILE__) . "/configuration.php");
-require_once(dirname(__FILE__) . "/include.php");
-
 function main() {
 	$g = new GalerieIndex();
 	$g->res_h_page();
 
 	Debug::afficher();
 }
-
-//trigger_error("ABCDE", E_USER_NOTICE);
-main();
 
 ?>
