19c8de60399123ed2453be34e88378e6cca186a5
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-10-05 16:14:36 +0200

    Tentative (échouée) de récupérer les erreurs de PHP :( .

--- cms2/code/index.php
+++ cms2/code/index.php
@@ -1,13 +1,19 @@
 <?php
 
 error_reporting(E_ALL | E_STRICT);
-// display_errors ne s'appliquera pas au fichier courant,
-// alors gardons-le aussi court que possible !
-ini_set("display_errors", 1);
+ini_set("display_errors", 1); // Ne s'appliquera pas au fichier courant !
 
-require_once(dirname(__FILE__) . "/configuration.php");
-require_once(dirname(__FILE__) . "/include.php");
+require_once(dirname(__FILE__) . "/debug.php");
 
-main();
+$old_error_handler = set_error_handler(array("Debug", "error_handler"), E_ALL | E_STRICT);
+$old_error_handler = set_error_handler(array("Debug", "error_handler"), E_ALL | E_STRICT);
+register_shutdown_function(array("Debug", "error_handler"));
+
+if ($old_error_handler === null) {
+	echo "<pre>Erreur lors de la mise en place des mécanismes de détection et d'affichage d'erreurs.</pre>";
+	die();
+}
+
+require_once(dirname(__FILE__) . "/main.php");
 
 ?>
--- cms2/code/main.php
+++ cms2/code/main.php
@@ -1,10 +1,16 @@
 <?php
 
+require_once(dirname(__FILE__) . "/configuration.php");
+require_once(dirname(__FILE__) . "/include.php");
+
 function main() {
 	$g = new GalerieIndex();
 	$g->res_h_page();
 
 	Debug::afficher();
 }
+
+//trigger_error("ABCDE", E_USER_NOTICE);
+main();
 
 ?>
